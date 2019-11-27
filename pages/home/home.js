b5832fa86624446087b02a848f7193dee8e26cc8
fuziwang <2622860598@qq.com>
2019-11-27 10:45:40 +0800

    home and the structure

---
+++ pages/home/home.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+    imgUrls: [
+      '../../images/swp_1.jpg',
+      '../../images/swp_2.jpg',
+      '../../images/swp_3.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 500,
+    circular:true
+  },
+})
